e47338743bcbcd3e421a25ab6a3434bfef2d363b
pradhummohan2-glitch <pradhummohan2@gmail.com>
2026-04-06 10:56:40 +0530

    Removed DB, using in-memory

--- app.py
+++ app.py
@@ -1,17 +1,10 @@
 from flask import Flask, request, jsonify
-import mysql.connector
 import os
 
 app = Flask(__name__)
 
-# Database connection
-def get_db_connection():
-    return mysql.connector.connect(
-        host=os.environ.get("MYSQL_HOST"),
-        user=os.environ.get("MYSQL_USER"),
-        password=os.environ.get("MYSQL_PASSWORD"),
-        database=os.environ.get("MYSQL_DATABASE")
-    )
+students = []
+current_id = 1
 
 # Validation
 def validate_student(data):
@@ -23,72 +16,48 @@ def validate_student(data):
         return "Age must be integer"
     return None
 
+# Home route
+@app.route('/')
+def home():
+    return "Student CRUD API is running 🚀"
+
 # CREATE
 @app.route('/students', methods=['POST'])
 def create_student():
+    global current_id
     data = request.get_json()
 
     error = validate_student(data)
     if error:
         return jsonify({"error": error}), 400
 
-    conn = get_db_connection()
-    cursor = conn.cursor()
+    data['id'] = current_id
+    current_id += 1
+    students.append(data)
 
-    query = "INSERT INTO student (name, email, age) VALUES (%s, %s, %s)"
-    cursor.execute(query, (data['name'], data['email'], data['age']))
-    conn.commit()
-
-    cursor.close()
-    conn.close()
-
-    return jsonify({"message": "Student added"}), 201
+    return jsonify(data), 201
 
 # READ
 @app.route('/students', methods=['GET'])
 def get_students():
-    conn = get_db_connection()
-    cursor = conn.cursor(dictionary=True)
-
-    cursor.execute("SELECT * FROM student")
-    students = cursor.fetchall()
-
-    cursor.close()
-    conn.close()
-
     return jsonify(students)
 
 # UPDATE
 @app.route('/students/<int:id>', methods=['PUT'])
 def update_student(id):
-    data = request.get_json()
-
-    conn = get_db_connection()
-    cursor = conn.cursor()
-
-    query = "UPDATE student SET name=%s, email=%s, age=%s WHERE id=%s"
-    cursor.execute(query, (data['name'], data['email'], data['age'], id))
-    conn.commit()
-
-    cursor.close()
-    conn.close()
-
-    return jsonify({"message": "Student updated"})
+    for s in students:
+        if s['id'] == id:
+            s.update(request.get_json())
+            return jsonify(s)
+    return {"error": "Not found"}, 404
 
 # DELETE
 @app.route('/students/<int:id>', methods=['DELETE'])
 def delete_student(id):
-    conn = get_db_connection()
-    cursor = conn.cursor()
+    global students
+    students = [s for s in students if s['id'] != id]
+    return {"message": "Deleted"}
 
-    cursor.execute("DELETE FROM student WHERE id=%s", (id,))
-    conn.commit()
-
-    cursor.close()
-    conn.close()
-
-    return jsonify({"message": "Student deleted"})
-
-# RUN APP
+# RUN
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
